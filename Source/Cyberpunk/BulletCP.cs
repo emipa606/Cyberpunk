ea73e699bc91eef12a8e0301517ddb57d48d9d2a
MDarque <mark.kochanowski@gmail.com>
2016-11-25 18:18:51 -0600

    Armors added.  Code modified for random weapon damage

---
+++ Source/Cyberpunk/BulletCP.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using Verse;
+using Verse.Sound;
+
+namespace Cyberpunk
+{
+    public class BulletCP : Projectile
+    {
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            if (hitThing != null)
+            {
+                int damageAmountMax = this.def.projectile.damageAmountBase;
+                int damageAmount = Rand.Range(0, damageAmountMax);
+
+                BodyPartDamageInfo value = new BodyPartDamageInfo(null, null);
+                DamageInfo dinfo = new DamageInfo(this.def.projectile.damageDef, damageAmount, this.launcher, this.ExactRotation.eulerAngles.y, new BodyPartDamageInfo?(value), this.equipmentDef);
+                hitThing.TakeDamage(dinfo);
+            }
+            else
+            {
+                SoundDefOf.BulletImpactGround.PlayOneShot(Position);
+                MoteMaker.MakeStaticMote(this.ExactPosition, ThingDefOf.Mote_ShotHit_Dirt, 1f);
+            }
+        }
+    }
+}
--- Source/Cyberpunk/Properties/AssemblyInfo.cs
+++ Source/Cyberpunk/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cyberpunk")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016 Diabolic Studios")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
